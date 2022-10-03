e4695d425770db49b643f37da1cb797e4ad358c9
Alexander Shar <alahan05@gmail.com>
2022-10-03 17:25:24 +0300

    Second task resolved

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Write numbers:");
+//int []array = {Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())};
+for(int n = 0; n < 3; n++)
+{
+    array[n] = Convert.ToInt32(Console.ReadLine());
+}
+
+for(int n = 0; n < 3; n++)
+{
+    if(n != 2 && array[n] > array[n+1])
+    {
+        array[n+1] = array[n];
+    }
+}
+System.Console.WriteLine($"Max value {array[2]}");
+
