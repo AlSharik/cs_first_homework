3545b1206cc018e9f7c5b2e0d178bcf5aecb3454
Alexander Shar <alahan05@gmail.com>
2022-10-03 17:08:28 +0300

    First task end

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Write numbers:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 > number2)
+{
+    Console.WriteLine($"max={number1} and min={number2}");
+}
+else if(number1 < number2)
+{
+    Console.WriteLine($"max={number2} and min={number1}");
+}
+else
+{
+    System.Console.WriteLine($"They are equals and value {number1}");
+}
