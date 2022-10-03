a18e0c7bfd7c9e36b1a08611e9887b9a027eff1e
Alexander Shar <alahan05@gmail.com>
2022-10-03 17:54:57 +0300

    4th task ended

---
+++ task4/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.WriteLine("Write number:");
+int number = Convert.ToInt32(Console.ReadLine());
+int even = 2;
+while(even <= number)
+{
+    System.Console.WriteLine(even);
+    even = even + 2;
+}
